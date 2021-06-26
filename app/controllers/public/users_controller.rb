94b52ffc6e8fa34c0ac6115177f72c7a3940da21
kznzu00 <mtstn5657@gmail.com>
2021-06-26 15:13:39 +0000

    update:ユーザー詳細のレイアウト調整

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -6,7 +6,8 @@ class Public::UsersController < ApplicationController
   end
 
   def show
-    @user =User.find(params[:id])
+    @user = User.find(params[:id])
+    @posts = @user.posts.page(params[:page]).reverse_order
   end
 
   def edit
